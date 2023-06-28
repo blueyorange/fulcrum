b896543019a7f20582a790c6339462406197bca8
Russell Johnson <blueyorange@gmail.com>
2023-06-28 14:05:23 +0100

    role changed to enum

--- models/User.js
+++ models/User.js
@@ -5,7 +5,11 @@ const UserSchema = new Schema({
   name: { type: String, required: true },
   givenName: { type: String, required: true },
   surname: { type: String, required: true },
-  role: { type: String, required: true },
+  role: {
+    type: String,
+    required: true,
+    enum: ["admin", "teacher", "student"],
+  },
   credentials: {
     access_token: { type: String },
     refresh_token: { type: String },
